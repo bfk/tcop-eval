05861f0129c88458724bc3906ab66cc064dd387b
Breandan Knowlton <b@breandan.org>
2018-01-17 13:14:52 -0500

    Parameterisation of the points page to start using values from a configuration json file

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var router = express.Router()
+var config = require('./tcop.json')
 
 // Route index page
 router.get('/', function (req, res) {
@@ -9,6 +10,10 @@ router.get('/', function (req, res) {
 // add your routes here
 router.get('/start-page', function (req, res) {
   res.render('start-page')
+})
+
+router.get('/1-define-user-needs', function (req, res) {
+  res.render('1-define-user-needs', { 'config': config, 'pointnum': 1 })
 })
 
 module.exports = router
