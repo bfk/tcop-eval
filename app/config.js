eebd4cdff2533a93115056f5d7a1cee8a954b6a5
Breandán Knowlton <b@breandan.org>
2018-05-01 15:31:24 +0100

    Updated content and labels based on user testing

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Tech Code of Practice Evaluation Tool',
+  serviceName: 'Check your technology project or programme',
 
   // Default port that prototype runs on
   port: '3000',
