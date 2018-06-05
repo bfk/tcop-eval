723c5bd6408994857473c4aa17751fc304d45644
Breandán Knowlton <b@breandan.org>
2018-06-05 17:56:30 +0100

    Installed GOV.UK Notify, started on email report.

--- app/routes.js
+++ app/routes.js
@@ -24,6 +24,29 @@ router.get('/settings', function (req, res) {
   res.render('settings', { 'config': config })
 })
 
+router.get('/confirmation-page', function (req, res) {
+  var apiKey = 'tcopteam-2e51730f-0653-4473-aa47-361810a7afad-ea1c8c26-8137-437d-9f95-8b5b8467d3e6'
+  var templateId = 'edb26f71-138e-4ff7-b4a3-3eeca41e1b9b'
+  var emailAddress = 'breandan.knowlton@digital.cabinet-office.gov.uk'
+  var personalisation = {
+    return_link: "http://example.com",
+    report: "* First thing\n* Second thing\nSome more things"
+  }
+  var reference = '12345'
+  var emailReplyToId = 'replyto@breandan.org'
+  var NotifyClient = require('notifications-node-client').NotifyClient,
+	notifyClient = new NotifyClient(apiKey)
+
+  notifyClient
+    .sendEmail(templateId, emailAddress, personalisation)
+    .then(response => console.log(response))
+    .catch(err => console.error(err))
+  ;
+
+  res.render('confirmation-page', { 'config': config })
+})
+
+
 
 
 module.exports = router
